f374bc5d84475ac3373db05e4966ee26318380cb
TechieTeee <100870737+TechieTeee@users.noreply.github.com>
2023-03-03 22:58:15 -0600

    Update PizzaData.sql

--- BitcoinDataAnalysis/PizzaData.sql
+++ BitcoinDataAnalysis/PizzaData.sql
@@ -1,1 +1,14 @@
--
+
+FROM
+`bigquery-public-data.bitcoin_blockchain.transactions`
+JOIN UNNEST (inputs) AS inputs
+JOIN UNNEST (outputs) AS outputs
+WHERE
+outputs.output_pubkey_base58 = 'ADDRESS'
+AND
+output_satoshis  >= 10000000
+AND
+timestamp < TIMESTAMP * 1000
+AND
+inputs.input_pubkey_base58 IS NOT NULL
+GROUP BY tt, input_key, output_key, btc
